176daf19172727100a0054e0db8d1f1872998672
Olawale <eyakubu442@gmail.com>
2026-02-21 17:22:54 +0100

    slight changes

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -16,7 +16,7 @@ const Menu = () => {
       <nav className='hidden md:flex w-full max-w-xs gap-1'>
         <ModeToggle />
         <Button asChild variant='ghost'>
-          <Link href='/cart'>
+          <Link href='/about'>
             <Store /> About
           </Link>
         </Button>
@@ -30,7 +30,7 @@ const Menu = () => {
             <SheetTitle>Menu</SheetTitle>
             <ModeToggle />
             <Button asChild variant='ghost'>
-              <Link href='/cart'>
+              <Link href='/about'>
                 <Store /> About
               </Link>
             </Button>
