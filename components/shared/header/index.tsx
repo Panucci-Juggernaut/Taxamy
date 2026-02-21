858f900d3f571282f937b1bdf2cf8ffe96f137d1
Olawale <eyakubu442@gmail.com>
2026-02-21 16:52:00 +0100

    added header, footer & themetoggle

---
+++ components/shared/header/index.tsx
@@ -0,0 +1,21 @@
+import { APP_NAME } from "@/lib/constants";
+import Link from "next/link";
+import Menu from "./menu";
+
+
+const Header = () => {
+   return <header className="w-full border-b">
+        <div className="wrapper flex-between">
+          <div className="flex-start">
+                <Link href={"/"} className="flex-start">
+                    <span className="hidden lg:block font-bold text-2xl ml-3">
+                        {APP_NAME} 
+                    </span>
+                </Link>
+            </div>
+            <Menu />
+        </div >
+    </header>;
+}
+
+export default Header
